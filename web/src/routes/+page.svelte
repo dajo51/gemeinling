eb80b9aa1010bf733b6be26f8f09ef6b8d2c2fdb
David Josiger <david.josiger@ida.me>
2024-01-13 19:21:41 +0100

    chore: added tailwind

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1 class="text-xl font-bold">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
