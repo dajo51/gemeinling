55bf020ad2beb8ae3912f14aaeb42b145e2cc56b
David Jo <david.josiger@gmail.com>
2024-01-21 11:47:59 +0100

    fix: added db url

--- web/src/lib/firebase/firebase.js
+++ web/src/lib/firebase/firebase.js
@@ -10,7 +10,8 @@ const firebaseConfig = {
 	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 	appId: import.meta.env.VITE_APP_ID,
-	measurementId: import.meta.env.VITE_MEASUREMENT_ID
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+	databaseURL: import.meta.env.VITE_DATABASE_URL
 };
 
 let firebaseApp;
