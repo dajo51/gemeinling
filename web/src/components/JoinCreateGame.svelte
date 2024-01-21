0b16fabc5bb62671a8dd9ccb54af815d09812cb2
David Jo <david.josiger@gmail.com>
2024-01-21 13:34:48 +0100

    feat: switched to realtime db

--- web/src/components/JoinCreateGame.svelte
+++ web/src/components/JoinCreateGame.svelte
@@ -1,33 +1,67 @@
 <script>
-	import { db } from '$lib/firebase/firebase';
-	import { doc, getDoc } from 'firebase/firestore';
+	import { realtimeDb } from '$lib/firebase/firebase';
+	import { ref, onValue, set } from 'firebase/database';
 	import { goto } from '$app/navigation';
 
 	let gameId = '';
 	let gameFound = '';
+	let playerNameMissing = false;
+	let playerName = '';
 	let lobbyData;
 
-	async function getLobby(lobbyId) {
-		try {
-			const docRef = doc(db, 'games', lobbyId);
-			const docSnap = await getDoc(docRef);
-			if (docSnap.exists()) {
+	const generateGameId = (length) => {
+		return Array(length)
+			.fill('x')
+			.join('')
+			.replace(/x/g, () => {
+				return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+			});
+	};
+
+	async function createRealtimeGameRoom({ playerName }) {
+		const gameId = generateGameId(8);
+
+		set(ref(realtimeDb, gameId), {
+			players: [{ name: playerName, points: 0 }],
+			createdAt: new Date()
+		});
+		return gameId;
+	}
+
+	async function getLobby(gameId) {
+		onValue(ref(realtimeDb, gameId), (snapshot) => {
+			const data = snapshot.val();
+			if (data) {
 				gameFound = 'found';
 			} else {
 				gameFound = 'notFound';
 			}
-		} catch (error) {
-			console.error('Error fetching data: ', error);
-		}
+		});
 	}
 
 	async function createLobby() {
-		await goto(`/new-game`);
+		if (playerName.trim() === '') {
+			console.error('Player name is required');
+			playerNameMissing = true;
+			return;
+		}
+
+		try {
+			const newGameId = await createRealtimeGameRoom({ playerName });
+			console.log(`New game room created with ID: ${newGameId}`);
+			await goto(`/game/${newGameId}`);
+		} catch (error) {
+			console.error('Failed to create game room:', error);
+		}
 	}
 
 	async function joinLobby() {
+		if (playerName.trim() === '') {
+			console.error('Player name is required');
+			playerNameMissing = true;
+			return;
+		}
 		await getLobby(gameId);
-
 		if (gameFound === 'found') {
 			await goto(`/game/${gameId}`);
 		}
@@ -41,6 +75,12 @@
 			type="text"
 			placeholder="Spiel ID"
 			bind:value={gameId}
+			class="border p-2 rounded w-full mb-4"
+		/>
+		<input
+			type="text"
+			placeholder="Dein Name"
+			bind:value={playerName}
 			class="border p-2 rounded w-full mb-4"
 		/>
 		<button class="bg-blue-500 text-white rounded p-2 w-full mb-2" on:click={joinLobby}
@@ -63,6 +103,14 @@
 				role="alert"
 			>
 				<span class="block sm:inline">Spiel gefunden! Verbinde...</span>
+			</div>
+		{/if}
+		{#if playerNameMissing === true}
+			<div
+				class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-2"
+				role="alert"
+			>
+				<span class="block sm:inline">Spielername ist erforderlich!</span>
 			</div>
 		{/if}
 	</div>
